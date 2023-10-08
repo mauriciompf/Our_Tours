d99b0ad8eca16e36d3cb59887be3212ef75e0f5c
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-10-08 17:22:54 -0400

    Update Tours.jsx

--- src/Tours.jsx
+++ src/Tours.jsx
@@ -1,24 +1,61 @@
-function Button({ children, onClick }) {
-  return <button onClick={onClick}>{children}</button>;
-}
+import React, { useEffect, useState } from "react";
+import Button from "./Button.jsx";
 
-export default async function Tours() {
-  try {
-    const res = await fetch("https://www.course-api.com/react-tours-project");
+export default function Tours() {
+  const url = "https://www.course-api.com/react-tours-project";
+  const [data, setData] = useState([]);
 
-    if (!res.ok) {
-      throw new Error(`Can't fetch data, error status: ${res.status}`);
+  async function getData() {
+    try {
+      const res = await fetch(url);
+      if (!res.ok) {
+        throw new Error(`Can't fetch data, error status: ${res.status}`);
+      }
+      const d = await res.json();
+      setData(d);
+    } catch (error) {
+      console.error("Error: ", error);
     }
-
-    const data = await res.json();
-    console.log(data);
-  } catch (error) {
-    console.error("Error: ", error);
   }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const [expanded, setExpanded] = useState([]);
+
+  function toggleExpand(index) {
+    setExpanded((prevExpanded) => {
+      const newExpanded = [...prevExpanded];
+      newExpanded[index] = !newExpanded[index];
+      return newExpanded;
+    });
+  }
+
+  const caracterLimit = 200;
+
+  const tours = data.map(({ id, image, name, price, info }, index) => (
+    <li key={id}>
+      <div>
+        <img src={image} alt={name} />
+        <p>Price: ${price}</p>
+      </div>
+      <h3>{name}</h3>
+      <p>
+        {expanded[index] ? info : `${info.substring(0, caracterLimit)}...`}
+        {info.length > caracterLimit && (
+          <a href="#" onClick={() => toggleExpand(index)}>
+            {expanded[index] ? " Show Less" : "Read More"}
+          </a>
+        )}
+      </p>
+      <Button>Not Interested</Button>
+    </li>
+  ));
 
   return (
     <section>
-      <Button>Not Interested</Button>
+      <ul>{tours}</ul>
     </section>
   );
 }
